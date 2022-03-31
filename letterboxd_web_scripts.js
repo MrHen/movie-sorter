28aed6f1de6c88f8f6d1530f1cbc0accbdc95a41
MrHen <github@mrhen.com>
2022-03-30 19:07:25 -0500

    Update diary

--- letterboxd_web_scripts.js
+++ letterboxd_web_scripts.js
@@ -1,3 +1,4 @@
+// LIST COMPARISON
 titles_a = []
 headers = document.querySelectorAll(".headline-2.prettify")
 for (let header of headers) {
@@ -15,3 +16,22 @@ for (let header of headers) {
         header.style = "background-color: #434393;"
     }
 }
+
+
+// FILM LIST COMPARISON
+titles_a = []
+headers = document.querySelectorAll(".listitem .poster")
+for (let header of headers) {
+    titles_a.push(header.attributes["data-film-id"].textContent)
+}
+JSON.stringify(titles_a)
+
+titles_a = JSON.parse()
+
+headers = document.querySelectorAll(".listitem .poster")
+for (let header of headers) {
+    if (titles_a.includes(header.attributes["data-film-id"].textContent)) {
+        header.style = "opacity: 0.1;"
+    }
+}
+
