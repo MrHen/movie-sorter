1889a7ddd8d43070e9a4ee1af53cb67d4a44af1d
MrHen <github@mrhen.com>
2023-04-12 17:17:31 -0500

    run_cycle_fixer to 171

--- sorter_3.py
+++ sorter_3.py
@@ -776,7 +776,7 @@ run_save()
 
 # FIX EVERYTHING
 run_save()
-run_cycle_fixer(verbose=False, max_cycles=500, start=100)
+run_cycle_fixer(verbose=False, max_cycles=500, start=150)
 run_save()
 
 #### UTILITIES
